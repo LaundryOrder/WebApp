b5af393fae4be7af782a61d4965b229130cbd441
Epix <git@exz.me>
2016-06-07 21:12:58 +0800

    workable new order

--- app/src/newControllers.coffee
+++ app/src/newControllers.coffee
@@ -1,13 +1,29 @@
 newControllers = angular.module 'newControllers', ['ngMaterial', 'ngMessages']
 newControllers.controller 'NewCtrl', ['$scope', '$http', '$location', '$mdToast',
   ($scope, $http, $location, $mdToast)->
+    $scope.isLoadingAvail = true
+    $scope.isLoadingPlacing = false
+    $http
+      method: 'GET'
+      url: '/avail'
+    .then (response)->
+      $scope.isLoadingAvail = false
+      $scope.avail = response.data.time
     $scope.order = {}
     $scope.door = {}
     $scope.has_door = false
+    $scope.avail = 0
+    $scope.postOrder = ->
+      if $scope.has_door
+        $scope.order.door = $scope.door
+      $scope.isLoadingPlacing = true
+      $http
+        method: 'POST'
+        url: '/order'
+        data: $scope.order
+      .then (response) ->
+        $scope.back()
+        $mdToast.show($mdToast.simple().textContent('You order has been placed!'))
     $scope.back = ->
       $location.path 'orders'
-    $scope.avail = '13:00'
-    $scope.postOrder = ->
-      $scope.back()
-      $mdToast.show($mdToast.simple().textContent('You order has been placed!'))
 ]
--- app/src/newControllers.js
+++ app/src/newControllers.js
@@ -5,16 +5,35 @@ newControllers = angular.module('newControllers', ['ngMaterial', 'ngMessages']);
 
 newControllers.controller('NewCtrl', [
   '$scope', '$http', '$location', '$mdToast', function($scope, $http, $location, $mdToast) {
+    $scope.isLoadingAvail = true;
+    $scope.isLoadingPlacing = false;
+    $http({
+      method: 'GET',
+      url: '/avail'
+    }).then(function(response) {
+      $scope.isLoadingAvail = false;
+      return $scope.avail = response.data.time;
+    });
     $scope.order = {};
     $scope.door = {};
     $scope.has_door = false;
-    $scope.back = function() {
-      return $location.path('orders');
+    $scope.avail = 0;
+    $scope.postOrder = function() {
+      if ($scope.has_door) {
+        $scope.order.door = $scope.door;
+      }
+      $scope.isLoadingPlacing = true;
+      return $http({
+        method: 'POST',
+        url: '/order',
+        data: $scope.order
+      }).then(function(response) {
+        $scope.back();
+        return $mdToast.show($mdToast.simple().textContent('You order has been placed!'));
+      });
     };
-    $scope.avail = '13:00';
-    return $scope.postOrder = function() {
-      $scope.back();
-      return $mdToast.show($mdToast.simple().textContent('You order has been placed!'));
+    return $scope.back = function() {
+      return $location.path('orders');
     };
   }
 ]);
